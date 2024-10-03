1182d4cb65afe0f6d625320991aeebd23879f0b7
sammie-maxx <samuelbabarere1@gmail.com>
2024-10-03 12:09:48 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -39,29 +39,29 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //owmner v card
-global.ytname = "YT: S4 SALMAN YT" //ur yt chanel name
-global.socialm = "GitHub: salmanytofficial" //ur github or insta name
-global.location = "Pakistan, Lahore, Sabzazar" //ur location
+global.ytname = "YT: S A M M I E" //ur yt chanel name
+global.socialm = "GitHub: " //ur github or insta name
+global.location = "Africa, Nigeria, Lagos " //ur location
 
 //new
-global.botname = 'XLICON-V4-MD' //ur bot name
-global.ownernumber = ['923184070915'] //ur owner number, dont add more than one
-global.ownername = '𝙎𝙖𝙡𝙢𝙖𝙣 𝘼𝙝𝙢𝙖𝙙' //ur owner name
-global.websitex = "https://youtube.com/@s4salmanyt"
-global.wagc = "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
+global.botname = 'S A M M I E_MD' //ur bot name
+global.ownernumber = ['2349136429929'] //ur owner number, dont add more than one
+global.ownername = '𝙎 A M M I E' //ur owner name
+global.websitex = " S A M M I E"
+global.wagc = " S A M M I E"
 global.themeemoji = '⛩'
 global.wm = "Xlicon Bot Inc."
-global.botscript = 'https://github.com/salmanytofficial/XLICON-V4-MD' //script link
-global.packname = "𝘼𝙃𝙃𝙈𝙄 𝙆𝙐𝙉"
-global.author = "MΛDΣ BY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿"
-global.creator = "923184070915@s.whatsapp.net"
+global.botscript = ' S A M M I E' //script link
+global.packname = "SAM MIE"
+global.author = "MΛDΣ BY *****"
+global.creator = "2349136429929@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923184070915"] // Premium User
+global.premium = ["2349136429929"] // Premium User
 
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
 global.typereply = 'v4' // reply type 'v1' => 'v4'
-global.autoblocknumber = '212' //set autoblock country code
+global.autoblocknumber = '' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.antidelete = false //set anti delete 
 
